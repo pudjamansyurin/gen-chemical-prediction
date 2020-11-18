c02c43da7e4f6c7e589130cc1b5bbae7114b55eb
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-11-18 16:53:51 +0700

    Added new production conditional logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
 use Laravel\Jetstream\Jetstream;
@@ -28,5 +30,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+
+        /* Production condition */
+        if (App::environment('production')) {
+            /* Fix cpanel mysql issue */
+            Builder::defaultStringLength(191);
+
+            // /* Fix laravel-mix manifest issue */
+            // $this->app->bind('path.public', function () {
+            //     $domain = env('APP_PROD_DOMAIN');
+            //     return base_path() . "/../{$domain}";
+            // });
+        }
     }
 }
