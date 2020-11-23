8b10e663c24b99e72c182ecfcda05ea8a814b816
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-11-23 16:57:04 +0700

    Deep understanding about ML

--- app/Console/Commands/TrainML.php
+++ app/Console/Commands/TrainML.php
@@ -7,26 +7,33 @@ use App\Models\Measurement;
 use Illuminate\Console\Command;
 use Rubix\ML\BootstrapAggregator;
 use Rubix\ML\CommitteeMachine;
+use Rubix\ML\CrossValidation\HoldOut;
 use Rubix\ML\CrossValidation\KFold;
 use Rubix\ML\CrossValidation\Metrics\Accuracy;
 use Rubix\ML\CrossValidation\Metrics\MeanAbsoluteError;
 use Rubix\ML\CrossValidation\Metrics\MeanSquaredError;
 use Rubix\ML\CrossValidation\Metrics\RSquared;
 use Rubix\ML\CrossValidation\Metrics\SMAPE;
+use Rubix\ML\CrossValidation\MonteCarlo;
 use Rubix\ML\CrossValidation\Reports\ErrorAnalysis;
 use Rubix\ML\Datasets\Labeled;
+use Rubix\ML\Extractors\CSV;
+use Rubix\ML\Other\Loggers\Screen;
 use Rubix\ML\Pipeline;
 use Rubix\ML\Regressors\Adaline;
+use Rubix\ML\Regressors\DummyRegressor;
 use Rubix\ML\Regressors\ExtraTreeRegressor;
 use Rubix\ML\Regressors\GradientBoost;
 use Rubix\ML\Regressors\KDNeighborsRegressor;
 use Rubix\ML\Regressors\KNNRegressor;
+use Rubix\ML\Regressors\MLPRegressor;
 use Rubix\ML\Regressors\RadiusNeighborsRegressor;
 use Rubix\ML\Regressors\RegressionTree;
 use Rubix\ML\Regressors\Ridge;
 use Rubix\ML\Regressors\SVR;
 use Rubix\ML\Transformers\L2Normalizer;
 use Rubix\ML\Transformers\MinMaxNormalizer;
+use Rubix\ML\Transformers\NumericStringConverter;
 
 class TrainML extends Command
 {
@@ -61,97 +68,56 @@ class TrainML extends Command
      */
     public function handle()
     {
-        $bases = collect(['CV 1103']);
-        $target = 'KV 40';
+        $formulas = $this->getFormulas('KV 40', ['CV 1103']);
 
-        $target = Measurement::where('name', $target)->first();
-
-        $q = $target->formulas()
-            ->with([
-                'materials',
-                'measurements' => function($q) use ($target) {
-                    $q->where('name', $target->name);
-                },
-            ]);
-        $bases->each(function($base) use(&$q) {
-            $q->whereHas('materials', function($q) use($base) {
-                $q->where('name', $base);
-            });
-        });
-        $formulas = $q->get();
-
-        $data = $formulas
-            ->pipe(function($items) {
-                $features = $items
-                                ->pluck('materials')
-                                ->map(function($item) {
-                                    return $item->pluck('id');
-                                })
-                                ->flatten()
-                                ->unique()
-                                ->values()
-                                ->sort();
-
-                $samples = $items
-                            ->pluck('materials')
-                            ->map(function($item) {
-                                return $item->pluck('pivot.value', 'id');
-                            })
-                            ->map(function($item) use($features) {
-                                return $features->mapWithKeys(function($feature) use($item) {
-                                    return [$feature => $item->get($feature, 0)];
-                                });
-                            });
-
-                $labels = $items
-                            ->pluck('measurements.0.pivot.value');
-
-                return (object) [
-                    'features' => $features->all(),
-                    'samples' => $samples->toArray(),
-                    'labels' => $labels->all()
-                ];
-            });
-
-        $this->info("Loading to memory...");
-        $dataset = new Labeled($data->samples, $data->labels);
+        $data = $this->makeData($formulas);
         // $this->table($data->features, $data->samples);
+
+        $this->info("Loading...");
+        $dataset = (new Labeled($data->samples, $data->labels))
+                        // ->apply(new NumericStringConverter())
+                        // ->apply(new L2Normalizer())
+                        ->randomize();
         // $this->table($data->features, $dataset->samples());
         // dd($dataset->describe());
         // dd($dataset->describeLabels());
+        // dd($dataset->numRows(), $dataset->numColumns());
 
-        // $estimator = new ExtraTreeRegressor();
-        // $estimator = new BootstrapAggregator($estimator, 10, 0.5);
-        $estimator = new CommitteeMachine([
-            new Adaline(),
-            new ExtraTreeRegressor(),
-            new GradientBoost(),
-            new KDNeighborsRegressor(),
-            new KNNRegressor(),
-            new RadiusNeighborsRegressor(),
-            new RegressionTree(),
-            new Ridge()
-        ]);
+        $estimator = new ExtraTreeRegressor();
+        $estimator = new BootstrapAggregator($estimator, 100, 0.5);
+        // $estimator = new CommitteeMachine([
+        //     new DummyRegressor(),
+        //     new Adaline(),
+            //     new ExtraTreeRegressor(),        // 5.74
+            //     new GradientBoost(),             // 8.07
+            //     new KDNeighborsRegressor(),      // 8.85
+            //     new KNNRegressor(),              // 6.28
+        //     new MLPRegressor(),
+            //     new RadiusNeighborsRegressor(),  // 8.46
+            //     new RegressionTree(),            // 7.14
+            //     new Ridge()                      // 6.79
+        // ]);
+        // $estimator->setLogger(new Screen());
 
         $estimator = new Pipeline([
+            new NumericStringConverter(),
             new L2Normalizer()
         ], $estimator);
 
         $this->info("Validating...");
         $validator = new KFold(10);
-        $score = $validator->test($estimator, $dataset, new SMAPE());
+        $score = $validator->test($estimator, $dataset, new MeanAbsoluteError());
         dd($score);
 
-        $this->info("Splitting...");
-        [$training, $testing] = $dataset->randomize()->stratifiedSplit(0.8);
-
         $this->info("Training...");
+        [$training, $testing] = $dataset->stratifiedSplit(0.8);
         $estimator->train($training);
         // dd($estimator->trained());
         // dd($estimator->experts());
         // dd($estimator->featureImportances());
+        // dd($estimator->steps());
 
-        $this->info("Predicting...");
+        $this->info("Cross-validating...");
         $predictions = $estimator->predict($testing);
 
         $report = new ErrorAnalysis();
@@ -163,5 +129,59 @@ class TrainML extends Command
         // dd($score);
 
         return 0;
+    }
+
+    private function getFormulas($targetName, $baseNames) {
+        $target = Measurement::where('name', $targetName)->first();
+        $bases = collect($baseNames);
+
+        $q = $target->formulas()
+            ->with([
+                'materials',
+                'measurements' => function($q) use ($target) {
+                    $q->where('name', $target->name);
+                },
+            ]);
+
+        $bases->each(function($base) use(&$q) {
+            $q->whereHas('materials', function($q) use($base) {
+                $q->where('name', $base);
+            });
+        });
+
+        return $q->get();
+    }
+
+    private function makeData($formulas) {
+        return $formulas->pipe(function($items) {
+                $features = $items
+                                ->pluck('materials')
+                                ->map(function($item) {
+                                    return $item->pluck('name');
+                                })
+                                ->flatten()
+                                ->unique()
+                                ->values();
+
+                $samples = $items
+                            ->pluck('materials')
+                            ->map(function($item) {
+                                return $item->pluck('pivot.value', 'name');
+                            })
+                            ->map(function($item) use($features) {
+                                return $features->mapWithKeys(function($feature) use($item) {
+                                    return [$feature => $item->get($feature, 0)];
+                                });
+                            });
+
+                $labels = $items
+                            ->pluck('measurements.0.pivot.value', 'name');
+
+                return (object) [
+                    'features' => $features->all(),
+                    'samples' => $samples->toArray(),
+                    'labels' => $labels->all()
+                ];
+            });
     }
 }
