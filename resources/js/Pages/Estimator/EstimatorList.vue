3ca210c1337a088912f203ced7e5a446303069d5
Puja Kusuma <pujakusumae@hotmail.com>
2021-02-18 09:42:16 +0700

    Expose sail and add eslitrc

--- resources/js/Pages/Estimator/EstimatorList.vue
+++ resources/js/Pages/Estimator/EstimatorList.vue
@@ -11,14 +11,24 @@
     :loading="fetching"
   >
     <template v-slot:[`item.name`]="{ item }">
-      <v-chip @click="edit(item)" :color="chip(item)" :small="denser" dark>{{ item.name }}</v-chip>
+      <v-chip @click="edit(item)" :color="chip(item)" :small="denser" dark>{{
+        item.name
+      }}</v-chip>
     </template>
-    <template v-slot:[`item.steps`]="{ item }">{{ item.steps ? "Yes" : "No" }}</template>
-    <template v-slot:[`item.ranked`]="{ item }">{{ item.ranked ? "Yes" : "No" }}</template>
-    <template v-slot:[`item.updated_at`]="{ item }">{{ item.updated_at | moment("from") }}</template>
+    <template v-slot:[`item.steps`]="{ item }">{{
+      item.steps ? "Yes" : "No"
+    }}</template>
+    <template v-slot:[`item.ranked`]="{ item }">{{
+      item.ranked ? "Yes" : "No"
+    }}</template>
+    <template v-slot:[`item.updated_at`]="{ item }">{{
+      item.updated_at | moment("from")
+    }}</template>
 
     <template #card="{ item }">
-      <v-btn :color="chip(item)" absolute right small tile top>{{ item.user.name }}</v-btn>
+      <v-btn :color="chip(item)" absolute right small tile top>{{
+        item.user.name
+      }}</v-btn>
 
       <v-card-text @click="edit(item)">
         <div class="overline">{{ item.updated_at | moment("from") }}</div>
@@ -36,19 +46,19 @@
 import { ModelListMixin } from "@/Mixins/Model";
 
 export default {
-  mixins: [ModelListMixin],
-  data() {
-    return {
-      headers: [
-        { text: "Name", value: "name" },
-        { text: "Description", value: "desc" },
-        { text: "Steps", value: "steps", align: "center" },
-        { text: "Ranked", value: "ranked", align: "center" },
-        // { text: "Creator", value: "user.name" },
-        { text: "UpdatedAt", value: "updated_at" }
-      ]
-    };
-  }
+    mixins: [ModelListMixin],
+    data() {
+        return {
+            headers: [
+                { text: "Name", value: "name" },
+                { text: "Description", value: "desc" },
+                { text: "Steps", value: "steps", align: "center" },
+                { text: "Ranked", value: "ranked", align: "center" },
+                // { text: "Creator", value: "user.name" },
+                { text: "UpdatedAt", value: "updated_at" }
+            ]
+        };
+    }
 };
 </script>
 
